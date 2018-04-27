b24b16221df9f377dbf4e8b1e0d7e40c4da50b06
Rubal Wanchoo <rubalwanchoo@gmail.com>
2018-04-27 13:25:30 -0400

    phoneletter

--- LeetCode/src/LeetCode/PhoneLetterComb.java
+++ LeetCode/src/LeetCode/PhoneLetterComb.java
@@ -24,8 +24,8 @@ public class PhoneLetterComb {
 	
 	
 	public static void main(String args[]) {
-		displayList(letterCombinations("23"));
-		//displayList(letterCombinations("2"));
+		//displayList(letterCombinations("23"));
+		displayList(letterCombinations("2"));
 		//displayList(letterCombinations("22"));
 	}
 	
@@ -33,7 +33,21 @@ public class PhoneLetterComb {
 		
 		List<String> myList = new ArrayList<String>();
 		
+		HashMap<Integer,String[]> map1 = new HashMap<Integer,String[]>();
 		
+		map1=initializeMap1(map1);
+		
+		
+		for(int i =0;i<digits.length();i++){//go through each digit
+
+			String[] digitsAlphabets = map1.get(Integer.parseInt(String.valueOf(digits.charAt(i))));
+
+			//displayList(Arrays.asList(digitsAlphabets));
+			
+			//displayList(Arrays.asList(digitsAlphabets));
+			myList = combine(myList,Arrays.asList(digitsAlphabets));
+			//displayList(myList);
+		}
 
 		
 		return myList;
@@ -41,7 +55,34 @@ public class PhoneLetterComb {
     }
 	
 	
-	
+	public static List<String> combine(List<String> myList,List<String> alphaList){
+		
+		List<String> returnList = new ArrayList<String>();
+		
+		if(myList.size()==0){
+			for(int i=0;i<alphaList.size();i++){
+				
+				returnList.add((String)alphaList.toArray()[i]);
+
+			}
+		}else{
+			
+			for(int i=0;i<alphaList.size();i++){
+				for(int j=0;j<myList.size();j++){
+					
+					//System.out.println("i-"+i+",j-"+j);
+					returnList.add((String)myList.toArray()[j]+(String)alphaList.toArray()[i]);
+
+				}
+			}
+
+		}
+
+				
+			
+		
+		return returnList;
+	}
 	
 	public static HashMap<Integer,String[]> initializeMap1(HashMap<Integer,String[]> myMap1){
 		
@@ -61,9 +102,9 @@ public class PhoneLetterComb {
 	public static void displayList(List<String> myList) {
 		
 		Iterator<String> it = myList.iterator();
-		
 		while(it.hasNext()) {
 			System.out.print(it.next()+" ");
 		}
+		System.out.println();
 	}
 }
